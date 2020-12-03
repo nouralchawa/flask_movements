d6c57865eac150bdec5741071ca96361923d6966
Nour <nouralchawa@example.com>
2020-12-04 00:39:36 +0100

    Llamada a creaalta

--- movements/views.py
+++ movements/views.py
@@ -1,6 +1,21 @@
 from movements import app
 from flask import render_template
+import csv
 
 @app.route('/')
 def listaMovimientos():
-    return render_template("movementsList.html", miTexto="Ya veremos si hay movimientos o no")
+    fIngresos = open("movements/data/basedatos.csv","r")
+    csvReader = csv.reader(fIngresos, delimiter=',',quotechar='"')
+    ingresos =list(csvReader)
+
+    sumador = 0
+    for ingreso in ingresos:
+        sumador += float(ingreso [2])
+
+    print(ingresos)
+
+    return render_template("movementsList.html", datos=ingresos, total=sumador)
+
+@app.route('/creaalta')
+def nuevoIngreso():
+    return 'Ya el miercoles te enseño el formulario'
