e9884fbc0080bbe5ac425a1f40a0f8a02f886b37
Nour <nouralchawa@example.com>
2020-12-10 00:33:44 +0100

    Grabar movimientos sin validar

--- movements/views.py
+++ movements/views.py
@@ -1,21 +1,27 @@
 from movements import app
-from flask import render_template
+from flask import render_template, request, url_for, redirect
 import csv
 
 @app.route('/')
-def listaMovimientos():
+def listaIngresos():
     fIngresos = open("movements/data/basedatos.csv","r")
     csvReader = csv.reader(fIngresos, delimiter=',',quotechar='"')
     ingresos =list(csvReader)
 
-    sumador = 0
-    for ingreso in ingresos:
-        sumador += float(ingreso [2])
-
     print(ingresos)
 
-    return render_template("movementsList.html", datos=ingresos, total=sumador)
+    total = 0
+    for ingreso in ingresos:
+        total += float(ingreso [2])
 
-@app.route('/creaalta')
+    return render_template("movementsList.html", datos=ingresos, total=total)
+
+@app.route('/creaalta', methods=['GET','POST'])
 def nuevoIngreso():
-    return 'Ya el miercoles te enseño el formulario'
+    if request.method == 'POST':
+        fIngresos = open("movements/data/basedatos.csv","a", newline="")
+        csvWriter = csv.writer(fIngresos, delimiter=',', quotechar='"')
+        csvWriter.writerow([request.form.get('fecha'), request.form.get('concepto'), request.form.get('cantidad')])
+        return redirect(url_for('listaIngresos'))
+
+    return render_template("alta.html")
